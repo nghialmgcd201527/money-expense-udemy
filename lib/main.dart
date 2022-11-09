c5b442747485e8e03eb4a56661f4d84b9f71d0a0
nghialmgcd201527 <lmnghia911@gmail.com>
2022-11-09 22:00:27 +0700

    lecture 87

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,11 @@ class MyHomePage extends StatelessWidget {
     ),
   ];
 
+  // String titleInput;
+  // String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,16 +62,26 @@ class MyHomePage extends StatelessWidget {
                 children: <Widget>[
                   TextField(
                     decoration: InputDecoration(labelText: 'Title'),
+                    controller: titleController,
+                    // onChanged: (val) {
+                    //   titleInput = val;
+                    // },
                   ),
                   TextField(
                     decoration: InputDecoration(labelText: 'Amount'),
+                    controller: amountController,
+                    // onChanged: (val) {
+                    //   amountInput = val;
+                    // },
                   ),
                   TextButton(
                     style: ButtonStyle(
                       foregroundColor: MaterialStateProperty.all(Colors.purple),
                     ),
                     child: Text('Add transaction'),
-                    onPressed: () {},
+                    onPressed: () {
+                      print(titleController.text);
+                    },
                   )
                 ],
               ),
