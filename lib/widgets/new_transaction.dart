cfcd43304eddd494c91b9f9c889737feb9ee5a9e
nghialmgcd201527 <lmnghia911@gmail.com>
2022-11-10 01:26:44 +0700

    lecture 92

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -11,6 +11,20 @@ class NewTransaction extends StatelessWidget {
 
   NewTransaction(this.addTx);
 
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addTx(
+      enteredTitle,
+      enteredAmount,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -22,6 +36,7 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
               controller: titleController,
+              onSubmitted: (_) => submitData(),
               // onChanged: (val) {
               //   titleInput = val;
               // },
@@ -32,19 +47,16 @@ class NewTransaction extends StatelessWidget {
               // onChanged: (val) {
               //   amountInput = val;
               // },
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(),
             ),
             TextButton(
               style: ButtonStyle(
                 foregroundColor: MaterialStateProperty.all(Colors.purple),
               ),
               child: Text('Add transaction'),
-              onPressed: () {
-                addTx(
-                  titleController.text,
-                  double.parse(amountController.text),
-                );
-              },
-            )
+              onPressed: submitData,
+            ),
           ],
         ),
       ),
